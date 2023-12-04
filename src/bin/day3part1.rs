760968738c8f596ce9637ac3bbb3466e184ee143
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2023-12-04 04:47:38 -0500

    d3p1: even more nesting?

--- src/bin/day3part1.rs
+++ src/bin/day3part1.rs
@@ -24,27 +24,30 @@ fn parse_input(input: &str) -> Vec<&str> {
 
 fn get_solution(s: &[&str]) -> u32 {
     let mut sum = 0;
-    for (i, row) in s.iter().enumerate() {
+    // for each row...
+    s.iter().enumerate().map(|(i, row)| {
+        // split into groups of digits and non-digits
         let groups = row
             .chars()
             .enumerate()
             .group_by(|(_, c)| c.is_ascii_digit());
-
-        for group in groups
+        // filter to just only digits
+        let digit_groups = groups
             .into_iter()
-            .filter_map(|(k, group)| k.then_some(group))
-        {
-            let group: Vec<_> = group.collect();
-            let has_neighbor = group.iter().any(|(j, _)| has_neighbor_symbol(s, i, *j));
-            let string = group.iter().map(|(_, c)| c).collect::<String>();
-            let num = string.parse::<u32>().unwrap();
-            if has_neighbor {
-                sum += num;
-            }
-            println!("{} {}", num, has_neighbor);
-        }
-    }
-    sum
+            .filter_map(|(k, group)| k.then_some(group));
+        // for each set of digits...
+        return digit_groups
+            .map(|group| group.collect::<Vec<_>>())
+            .filter(|group| group.iter().any(|(j, _)| has_neighbor_symbol(s, i, *j)))
+            .map(|group| {
+                group
+                    .iter()
+                    .map(|(_, c)| c.to_digit(10).unwrap())
+                    .reduce(|acc, d| acc * 10 + d)
+                    .unwrap()
+            })
+            .sum::<u32>();
+    }).sum()
 }
 
 /// given a location, check whether any symbol is adjacent
@@ -55,4 +58,3 @@ fn has_neighbor_symbol(s: &[&str], r: usize, c: usize) -> bool {
         .filter_map(|(i, j)| s.get(i).and_then(|row| row.chars().nth(j)))
         .any(|c| c != '.' && !c.is_ascii_digit())
 }
-
